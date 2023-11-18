49ca05142491d385448ab2ae679588d1d51b9de6
Shanmukh025 <shanmukh2564@gmail.com>
2023-11-18 16:35:38 +0530

    Working

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,6 +1,12 @@
-import React from 'react';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import React, { useEffect } from 'react';
 
 const Header = () => {
+    useEffect(() => {
+        AOS.init();
+    }, []);
+
     return (
         <>
             <div className="heading">
