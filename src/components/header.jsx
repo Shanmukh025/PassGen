f9c43b49b5f0343e0fe1d476efc642d6902ad119
Shanmukh025 <shanmukh2564@gmail.com>
2023-11-18 16:22:33 +0530

    commit

---
+++ src/components/header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <>
+            <div className="heading">
+                <h1>PASSWORD GENERATOR</h1>
+                <p><i>Create Strong and Secure Passwords and PIN to keep your account safe.</i></p>
+            </div>
+        </>
+    );
+}
+
+export default Header;
